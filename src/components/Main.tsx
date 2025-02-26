290c9d24e5d2f611556ad738440302a2c2698d18
tristns <tristanflounders@gmail.com>
2025-02-26 16:48:56 +1000

    Updated portfolio with my details

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="https://drive.google.com/file/d/1kZlTjpdKATDMP_sdfytIkxSB8m3nq-md/view?usp=drive_link" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Tristan Flounders</h1>
+          <p>Undergraduate Chemical Process Engineer and Physist</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/tristns" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/tristan-flounders-1bbbb6208/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
